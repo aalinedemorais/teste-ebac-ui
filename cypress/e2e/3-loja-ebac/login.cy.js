2b6652162140349f78d2b1bbd199ca97f0585a23
Aline Morais <morais.alinede@gmail.com>
2025-04-04 09:33:17 -0300

    testes automatizados com massa de dados

--- cypress/e2e/3-loja-ebac/login.cy.js
+++ cypress/e2e/3-loja-ebac/login.cy.js
@@ -1,29 +1,52 @@
 /// <reference types="cypress"/>
+const perfil = require('../../fixtures/perfil.json')
 
 describe('Funcionalidade: Login', () => {
 
     beforeEach(() => {
-        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+        cy.visit('minha-conta')
     })
     afterEach(() => {
         cy.screenshot()
     });
-it('Deve fazer login com sucesso', () => {
-    cy.get('#username').type('amora@uorak.com')
-    cy.get('#password').type('teste123')
-    cy.get('.woocommerce-form > .button').click()
-    cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, amora (não é amora? Sair)')
-})
-it('Deve exibir mensagem de erro ao inserir usuário inválido', () => {
-    cy.get('#username').type('teste@uorak.com')
-    cy.get('#password').type('teste123')
-    cy.get('.woocommerce-form > .button').click()
-    cy.get('.woocommerce-error > li').should('exist')
-})
-it('Deve exibir mensagem de erro ao inserir senha inválida', () => {
-    cy.get('#username').type('amora@uorak.com')
-    cy.get('#password').type('teste')
-    cy.get('.woocommerce-form > .button').click()
-    cy.get('.woocommerce-error > li').should('contain', 'Erro: A senha fornecida para o e-mail')
-})
+
+    it('Deve fazer login com sucesso', () => {
+        cy.get('#username').type('amora@uorak.com')
+        cy.get('#password').type('teste123')
+        cy.get('.woocommerce-form > .button').click()
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, amora (não é amora? Sair)')
+    });
+
+    it('Deve exibir mensagem de erro ao inserir usuário inválido', () => {
+        cy.get('#username').type('teste@uorak.com')
+        cy.get('#password').type('teste123')
+        cy.get('.woocommerce-form > .button').click()
+        cy.get('.woocommerce-error > li').should('exist')
+    });
+
+    it('Deve exibir mensagem de erro ao inserir senha inválida', () => {
+        cy.get('#username').type('amora@uorak.com')
+        cy.get('#password').type('teste')
+        cy.get('.woocommerce-form > .button').click()
+        cy.get('.woocommerce-error > li').should('contain', 'Erro: A senha fornecida para o e-mail')
+    });
+
+    //massa de dados puxando de um arquivo
+    it('Deve fazer login com sucesso usando massa de dados', () => {
+        cy.get('#username').type(perfil.usuario)
+        cy.get('#password').type(perfil.senha)
+        cy.get('.woocommerce-form > .button').click()
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, amora (não é amora? Sair)')
+    });
+
+    //massa de dados pelo método nativo cypress
+    it.only('Deve fazer login com sucesso usando fixture', () => {
+        cy.fixture('perfil').then( dados => {
+            cy.get('#username').type(dados.usuario)
+            cy.get('#password').type(dados.senha , {log: false})
+            cy.get('.woocommerce-form > .button').click()
+            cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, amora (não é amora? Sair)')   
+        })
+    });
+
 })
