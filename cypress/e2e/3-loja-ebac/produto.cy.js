0e65f7208739b5299b28d86e1ac90baf0215ba1c
Aline Morais <morais.alinede@gmail.com>
2025-04-14 09:46:00 -0300

    testes com massa de dados em lista

--- cypress/e2e/3-loja-ebac/produto.cy.js
+++ cypress/e2e/3-loja-ebac/produto.cy.js
@@ -40,11 +40,31 @@ describe('Funcionalidade: Produtos', () => {
     });
 
     it('Deve visitar a página do produto', () => {
-
+        // usado com hífen normalmente
+        // produtosPage.visitarProduto('Arcadio-Gym-Short')
+        // para usar com espaço tem que ter urlFormatada no produtos.page.js
+        produtosPage.visitarProduto('Arcadio Gym Short')
+        cy.get('.product_title').should('contain', 'Arcadio Gym Short')
     });
 
     it('Deve adicionar produto ao carrinho', () => {
+        produtosPage.buscarProduto('Ariel Roll Sleeve Sweatshirt')
+        // forma de fazer sem dados parametrizáveis
+        // produtosPage.addProdutoCarrinho()
+        let qtd = 7
+        produtosPage.addProdutoCarrinho('XL', 'Green', qtd)
+        cy.get('.woocommerce-message').should('contain', qtd + ' × “Ariel Roll Sleeve Sweatshirt” foram adicionados no seu carrinho.')
+    });
 
+    it.only('Deve adicionar produto ao carrinho buscando da massa de dados', () => {
+        cy.fixture('produtos').then(dados => {
+            produtosPage.buscarProduto(dados[1].nomeProduto)
+            produtosPage.addProdutoCarrinho0(
+                dados[1].tamanho,
+                dados [1].cor,
+                dados[1].quantidade)
+            cy.get('.woocommerce-message').should('contain', dados[1].nomeProduto)
+        })        
     });
 
 });
--- cypress/support/page-objects/produtos.page.js
+++ cypress/support/page-objects/produtos.page.js
@@ -14,12 +14,21 @@ class ProdutosPage {
             .click()
     }
 
-    visitarProduto() {
-
+    visitarProduto(nomeProduto) {
+        // cy.visit(`produtos/${nomeProduto}`)
+        const urlFormatada = nomeProduto.replace(/ /g, '-')
+        cy.visit(`produtos/${urlFormatada}`)
     }
 
     addProdutoCarrinho() {
+        // cy.get('.button-variable-item-M').click()
+        // cy.get('.button-variable-item-Green').click()
 
+        // para fazer com dados parametrizáveis juntando string+parâmetro, exemplificado abaixo duas formas de fazer a mesma coisa
+        cy.get('.button-variable-item-' + tamanho).click()
+        cy.get(`.button-variable-item-${cor}`).click()
+        cy.get('.input-text').clear().type(quantidade)
+        cy.get('.single_add_to_cart_button').click()
     }
 
 }
