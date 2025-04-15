b2d7c39de8f0af93b7bbabfb6b18b3955b78de97
Aline Morais <morais.alinede@gmail.com>
2025-04-15 08:34:50 -0300

    mudanca no gitignore

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,13 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: "ffb596",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
     baseUrl: 'http://lojaebac.ebaconline.art.br/',
+    video: true,
   },
+  
 });
